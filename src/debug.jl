f27921e166d609d00166adc61dd70b9a28839d85
fps <mista.tapas@gmx.net>
2025-04-21 20:30:27 +0200

    Update debug.jl

--- src/debug.jl
+++ src/debug.jl
@@ -2,7 +2,7 @@
 
 function isdebug(group, mod=CUDA)
     logger = Base.CoreLogging.current_logger_for_env(Base.CoreLogging.Debug, group, mod)
-    shouldLog = logger !== nothing && Base.Logging.shouldLog(logger, Base.CoreLogging.Debug, mod, group, 0)
+    shouldLog = logger !== nothing && Logging.shouldLog(logger, Base.CoreLogging.Debug, mod, group, 0)
     println(shouldLog)
     shouldLog
 end
